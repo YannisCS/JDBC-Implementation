5b9a8b742870dac9de40195f2697664b14e0beb9
Yannis <13587224594@163.com>
2025-03-31 12:23:48 -0700

    add price in Items

--- CS5200Project/src/main/java/game/model/Items.java
+++ CS5200Project/src/main/java/game/model/Items.java
@@ -1,18 +1,23 @@
 package game.model;
 
+import java.math.BigDecimal;
+import java.util.Objects;
+
 public class Items {
 	
 	private int itemID;
 	private String itemName;
 	private int level;
 	private int maxStackSize;
+	private BigDecimal price;
 	
-	public Items(int itemID, String itemName, int level, int maxStackSize) {
+	public Items(int itemID, String itemName, int level, int maxStackSize, BigDecimal price) {
 		super();
 		this.itemID = itemID;
 		this.itemName = itemName;
 		this.level = level;
 		this.maxStackSize = maxStackSize;
+		this.price = price;
 	}
 
 	public int getItemID() {
@@ -47,11 +52,44 @@ public class Items {
 		this.maxStackSize = maxStackSize;
 	}
 
+	public BigDecimal getPrice() {
+		return price;
+	}
+
+	public void setPrice(BigDecimal price) {
+		this.price = price;
+	}
+
+	@Override
+	public int hashCode() {
+		throw new UnsupportedOperationException("hashing not supported");
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Items other = (Items) obj;
+		return itemID == other.itemID && Objects.equals(itemName, other.itemName) && level == other.level
+				&& maxStackSize == other.maxStackSize && Objects.equals(price, other.price);
+	}
+
 	@Override
 	public String toString() {
-		return "Items [itemID=" + itemID + ", itemName=" + itemName + ", level=" + level + ", maxStackSize="
-				+ maxStackSize + "]";
+		return String.format(
+				"Items [itemID=%s, itemName=%s, level=%s, maxStackSize=%s, price=%s]", 
+				itemID, 
+				itemName,
+				level, 
+				maxStackSize, 
+				price);
 	}
+
+	
 	
 	
 
