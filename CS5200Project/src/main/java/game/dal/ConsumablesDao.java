02629638f02092a9b60a7ee587b3de6fd7e67fde
Yannis <13587224594@163.com>
2025-03-31 12:56:09 -0700

    update consumables and consumablesDao

--- CS5200Project/src/main/java/game/dal/ConsumablesDao.java
+++ CS5200Project/src/main/java/game/dal/ConsumablesDao.java
@@ -2,13 +2,66 @@ package game.dal;
 
 import game.model.*;
 
+import java.math.BigDecimal;
 import java.sql.*;
 import java.util.*;
 
 public class ConsumablesDao {
+
 	/**
 	 * create rows in both Items and Consumables tables
 	 */
+	public static Consumables create(
+			Connection cxn,
+			String itemName,
+			int level,
+			int maxStackSize,
+			BigDecimal price,
+			String description
+	) throws SQLException {
+		int itemID = ItemsDao.create(cxn, itemName, level, maxStackSize, price);
+		String insertConsumables = "INSERT INTO Consumables (itemID, description) VALUES (?, ?,);";
+		
+		try (PreparedStatement pstmt = cxn.prepareStatement(insertConsumables)) {
+			pstmt.setInt(1, itemID);
+			pstmt.setString(2, description);
+			
+			pstmt.executeUpdate();
+			
+			return new Consumables(itemID, itemName, level, maxStackSize, price, description);
+		}
+	}
+	
+	/**
+	 * update an existing Consumables record (description) in the database
+	 */
+	public static Consumables updateConsumablesDescription(
+			Connection cxn,
+			Consumables old,
+			String newDescription
+	) throws SQLException {
+		String query_updateConsumables = """
+				UPDATE Consumables
+				SET description = ?
+				WHERE itemID = ?;
+				""";
+		
+		try (PreparedStatement pstmt = cxn.prepareStatement(query_updateConsumables)) {
+			pstmt.setString(1, newDescription);
+			pstmt.setInt(2, old.getItemID());
+			
+			pstmt.executeUpdate();
+			
+			return new Consumables(
+					old.getItemID(),
+					old.getItemName(),
+					old.getLevel(),
+					old.getMaxStackSize(),
+					old.getPrice(),
+					newDescription
+					);
+		}
+	}
 	
 
 }
--- CS5200Project/src/main/java/game/model/Consumables.java
+++ CS5200Project/src/main/java/game/model/Consumables.java
@@ -1,6 +1,7 @@
 package game.model;
 
 import java.math.BigDecimal;
+import java.util.Objects;
 
 public class Consumables extends Items{
 	private String description;
@@ -15,11 +16,45 @@ public class Consumables extends Items{
 		super(itemID, itemName, level, maxStackSize, price);
 		this.description = description;
 	}
-	
+    
 	//getters and setter
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + Objects.hash(description);
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Consumables other = (Consumables) obj;
+		return Objects.equals(description, other.description);
+	}
+
+	@Override
+	public String toString() {
+		return String.format(
+				"%s %nitemType: Consumables [description=%s]", 
+				super.toString(),
+				description
+				);
+	}
 	
-	//hashCode and equals
 	
-	//toString
 
 }
