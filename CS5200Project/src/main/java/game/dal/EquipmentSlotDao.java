20c7fcad9f23b445537e2c97fb9722adf2851fd4
wenjingLiumi <winjane2024@163.com>
2025-03-31 12:38:57 -0700

    update EquipmentSlotDao

--- CS5200Project/src/main/java/game/dal/EquipmentSlotDao.java
+++ CS5200Project/src/main/java/game/dal/EquipmentSlotDao.java
@@ -1,5 +1,28 @@
 package game.dal;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import game.model.EquipmentSlot;
+
+
+
 public class EquipmentSlotDao {
+	
+	public static EquipmentSlot create( Connection cxn,
+	        String bodyPartName
+	      ) throws SQLException{
+		
+	    String insertEquipmentSlot = "INSERT INTO EquipmentSlot (bodyPartName) VALUES (?);";
+	    
+	    try (PreparedStatement insertStmt = cxn.prepareStatement(insertEquipmentSlot)) {
+	    	
+	        insertStmt.setString(1, bodyPartName);
+	        insertStmt.executeUpdate();
+	        return new EquipmentSlot(bodyPartName);
+	      }
+	}
+	
 
 }
--- CS5200Project/src/main/java/game/model/Items.java
+++ CS5200Project/src/main/java/game/model/Items.java
@@ -1,6 +1,5 @@
 package game.model;
 
-import java.math.BigDecimal;
 import java.util.Objects;
 
 public class Items {
